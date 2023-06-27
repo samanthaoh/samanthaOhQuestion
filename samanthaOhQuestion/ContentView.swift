12e0df46373afc10ba90f0c3fdb1777a5ccd7df5
samanthaoh <samanthagrace1818@gmail.com>
2023-06-27 11:17:24 -0400

    Initial Commit

---
+++ samanthaOhQuestion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  samanthaOhQuestion
+//
+//  Created by Samantha Oh on 6/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ samanthaOhQuestion/samanthaOhQuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  samanthaOhQuestionApp.swift
+//  samanthaOhQuestion
+//
+//  Created by Samantha Oh on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct samanthaOhQuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
